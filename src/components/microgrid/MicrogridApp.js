da73a1929eabe703b543f772159974ad5f54c530
Pawel Lisewski <pawel.lisewski@gmail.com>
2017-07-12 11:25:31 +0800

    move chart size to state of MicrogridChart

--- src/components/microgrid/MicrogridApp.js
+++ src/components/microgrid/MicrogridApp.js
@@ -5,14 +5,11 @@ import MicrogridChart from './MicrogridChart.js';
 import MicrogridGraphic from './MicrogridGraphic.js';
 import ChartControls from './ChartControls.js';
 
-const margin = {top: 30, bottom: 30, left: 60, right: 30};
-
 class MicrogridApp extends React.Component {
   constructor (props) {
     super(props);
     this.state = {
       data: [],
-      size: { width: 0, height: 0 },
       timeline: new TimelineMax({paused: true, onUpdate: this.updateSlider.bind(this)}),
       progress: 0
     };
@@ -30,20 +27,29 @@ class MicrogridApp extends React.Component {
   }
 
   componentDidMount () {
-    let w = this.app.offsetWidth - margin.left - margin.right;
-    let h = this.app.offsetWidth * 0.75 - margin.top - margin.bottom;
-    this.setState({size: {width: w, height: h}});
-
     getSolarIrradiance('2010-06-01')
       .then(csv => this.setState({data: csv}));
   }
 
   render () {
     return (
-      <div id="microgrid-app" ref={app => this.app = app}>
-        <ChartControls tl={this.state.timeline} progress={this.state.progress} handleRangeChange={this.handleRangeChange}/>
-        <MicrogridChart {...this.state} {...this.state.size} margin={margin}/>
-        <MicrogridGraphic {...this.state}/>
+      <div id="microgrid-app">
+
+        <div className="columns">
+          <div className="column">
+            <ChartControls tl={this.state.timeline} progress={this.state.progress} handleRangeChange={this.handleRangeChange}/>
+          </div>
+        </div>
+
+        <div className="columns">
+          <div className="column is-half">
+            <MicrogridChart {...this.state}/>
+          </div>
+          <div className="column is-half">
+            <MicrogridGraphic {...this.state}/>
+          </div>
+        </div>
+
       </div>
     );
   }
--- src/components/microgrid/MicrogridChart.js
+++ src/components/microgrid/MicrogridChart.js
@@ -5,49 +5,69 @@ import LineChart from './LineChart.js';
 import XYaxis from './XYaxis.js';
 import ChartAnimationOverlay from './ChartAnimationOverlay.js';
 
-const svgSize = props => {
-  return {
-    width: props.width + props.margin.left + props.margin.right,
-    height: props.height + props.margin.top + props.margin.bottom
-  };
-};
+class MicrogridChart extends React.Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      width: 400,
+      height: 300,
+      margin: { top: 30, bottom: 30, left: 60, right: 30}
+    };
+  }
 
-const transform = props => `translate(${props.margin.left}, ${props.margin.bottom})`;
+  componentDidMount () {
+    let {margin} = this.state;
+    let w = this.chart.offsetWidth - margin.left - margin.right;
+    let h = this.chart.offsetWidth * 0.75 - margin.top - margin.bottom;
+    this.setState({width: w, height: h});
+  }
 
-const x = props => { return d3.scaleTime()
-  .domain(d3.extent(props.data, d => d.date))
-  .range([0, props.width]);
-};
+  render () {
+    const {data, timeline} = this.props;
+    const state = this.state;
+    const scales = {x: x(state.width, data), y: y(state.height, data)};
 
-const y = props => { return d3.scaleLinear()
-  //.domain([0, d3.max(props.data, d => d.value) + 100])
-  .domain([0, 1500])
-  .range([props.height, 0]);
-};
+    return (
+      <div className="card" ref={(chart) => this.chart = chart}>
+        <svg {...svgSize(state)}>
+          <g transform={transform(state)}>
+            <LineChart timeline={timeline} data={data} {...scales}/>
+            <ChartAnimationOverlay timeline={timeline} height={state.height} width={state.width}/>
+            <XYaxis width={state.width} height={state.height} {...scales}/>
+          </g>
+        </svg>
+      </div>
+    );
+  }
+}
 
 MicrogridChart.propTypes = {
   data: PropTypes.array.isRequired,
-  width: PropTypes.number.isRequired,
-  height: PropTypes.number.isRequired,
   timeline: PropTypes.object.isRequired
 };
 
+export default MicrogridChart;
 
-function MicrogridChart (props) {
-  const scales = {x: x(props), y: y(props)};
-
-  return (
-    <div className="card">
-      <svg {...svgSize(props)}>
-        <g transform={transform(props)}>
-          <LineChart timeline={props.timeline} data={props.data} {...scales}/>
-          <ChartAnimationOverlay timeline={props.timeline} height={props.height} width={props.width}/>
-          <XYaxis width={props.width} height={props.height} {...scales}/>
-        </g>
-      </svg>
-    </div>
-  );
+function svgSize(state) {
+  return {
+    width: state.width + state.margin.left + state.margin.right,
+    height: state.height + state.margin.top + state.margin.bottom
+  };
 }
 
+function transform(state) {
+  return `translate(${state.margin.left}, ${state.margin.bottom})`;
+}
 
-export default MicrogridChart;
+function x(width, data) {
+  return d3.scaleTime()
+    .domain(d3.extent(data, d => d.date))
+    .range([0, width]);
+}
+
+function y(height, data) {
+  return d3.scaleLinear()
+  //.domain([0, d3.max(data, d => d.value) + 100])
+    .domain([0, 1500])
+    .range([height, 0]);
+}
