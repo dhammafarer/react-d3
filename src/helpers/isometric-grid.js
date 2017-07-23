2c0beb93481b9bd4bfd1a53ac60df8d0080fae9d
Pawel Lisewski <pawel.lisewski@gmail.com>
2017-07-23 21:40:57 +0800

    write passing test for isometricTilePolygonPoints

--- src/helpers/isometric-grid.js
+++ src/helpers/isometric-grid.js
@@ -1,12 +1,29 @@
-export function getIsometricTileCoords (gridSize, tileSize) {
-  return Array.apply(null, Array(gridSize[1]))
-    .map(() => Array.apply(null, Array(gridSize[0])))
+export function isometricTileCoords ([rows, cols], [tileWidth, tileHeight]) {
+  return Array.apply(null, Array(cols))
+    .map(() => Array.apply(null, Array(rows)))
     .map((row, i) => row
       .map((el, j) => {
         return {
-          x: (tileSize[0] / 2) * (gridSize[1] - 1 - i + j),
-          y: (tileSize[1] / 2) * (i + j)
+          x: (tileWidth / 2) * (cols - 1 - i + j),
+          y: (tileHeight / 2) * (i + j)
         };
       })
     );
+}
+
+export function isometricSvgTilePoints (tileSize, tileCoordsTable) {
+  return flatten(tileCoordsTable).map(tile => getIsoTilePolygonPoints(tileSize, tile));
+}
+
+function flatten (array) {
+  return array.reduce((a,b) => a.concat(b), []);
+}
+
+function  getIsoTilePolygonPoints ([width, height], {x, y}) {
+  return [
+    [x + width / 2, y],
+    [x + width, y + height / 2],
+    [x + width / 2, y + height],
+    [x, y + height / 2]
+  ].join(' ');
 }
--- src/helpers/isometric-grid.test.js
+++ src/helpers/isometric-grid.test.js
@@ -1,15 +1,28 @@
 /* global describe it expect */
-import { getIsometricTileCoords } from './isometric-grid.js';
+import { isometricTileCoords, isometricSvgTilePoints } from './isometric-grid.js';
 
-describe('getIsometricTileCoords', () => {
+describe('isometricTileCoords', () => {
   it('outputs an array of isometric coords', () => {
     let tileSize = [200, 100];
     let gridSize = [2, 2];
-    let result = getIsometricTileCoords(gridSize, tileSize);
+    let result = isometricTileCoords(gridSize, tileSize);
     let expected = [
       [{x: 100, y: 0}, {x: 200, y: 50}],
       [{x: 0, y: 50}, {x: 100, y: 100}]
     ];
+
+    expect(result).toEqual(expected);
+  });
+});
+
+describe('isometricSvgTilePoints', () => {
+  it('given tile position, returns an array of polygon points', () => {
+    let tileSize = [200, 100];
+    let tileCoordsTable = [
+      [{x: 100, y: 0}]
+    ];
+    let result = isometricSvgTilePoints(tileSize, tileCoordsTable);
+    let expected = ['200,0 300,50 200,100 100,50'];
 
     expect(result).toEqual(expected);
   });
