20cc05cc4645ca6069419b11574bba16e05f8b99
Pawel Lisewski <pawel.lisewski@gmail.com>
2017-07-24 11:09:54 +0800

    add snapshot test for MicrogridGraphic

---
+++ src/components/microgrid/MicrogridGraphic.test.js
@@ -0,0 +1,23 @@
+/* globals describe, it, expect*/
+import React from 'react';
+import renderer from 'react-test-renderer';
+import MicrogridGraphic from './MicrogridGraphic.js';
+
+describe('MicrogridGraphic', () => {
+  it('displays the graphic', () => {
+    const options = {createNodeMock};
+    const tree = renderer.create(
+      <MicrogridGraphic />,
+      options
+    );
+
+    expect(tree).toMatchSnapshot();
+  });
+});
+
+function createNodeMock (element) {
+  if (element.type === 'div') {
+    return {style: {}};
+  }
+  return null;
+}
