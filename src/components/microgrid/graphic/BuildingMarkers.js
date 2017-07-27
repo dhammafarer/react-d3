8066ba42107269c3ff8635f5e8e5fecafafbf129
Pawel Lisewski <pawel.lisewski@gmail.com>
2017-07-27 17:12:03 +0800

    fix marker sizing

--- src/components/microgrid/graphic/BuildingMarkers.js
+++ src/components/microgrid/graphic/BuildingMarkers.js
@@ -9,15 +9,29 @@ BuildingMarkers.propTypes = {
 function BuildingMarkers ({data, handleClick}) {
   return (
     <div className="markers">
-      {data.map((el) =>
-        <div key={el.pos} className="tile" style={el.style}>
-          <div className="marker-tile">
-            <div className="dot-background"/>
-            <div className="dot" onClick={(e) => handleClick(e, el)}/>
+      {data.map((el) => {
+        return (
+          <div key={el.pos} className="tile" style={convertStyle(el)}>
+            <div className="marker-tile">
+              <div className="dot-background"/>
+              <div className="dot" onClick={(e) => handleClick(e, el)}/>
+            </div>
           </div>
-        </div>)}
+        );
+      })}
     </div>
   );
 }
 
 export default BuildingMarkers;
+
+function convertStyle (el) {
+  let height = el.style.height / el.texture.height;
+  let top = el.style.top + (el.texture.offsetHeight * height);
+  return {
+    height,
+    top,
+    left: el.style.left,
+    width: el.style.width
+  };
+}
